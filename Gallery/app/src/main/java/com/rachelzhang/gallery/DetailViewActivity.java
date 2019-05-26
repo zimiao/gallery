b2d3b5c1acc2d70e963ded45b09f870da8d4fd25
rachelzhang <zzy.rachel@gmail.com>
2019-05-26 15:41:35 -0700

    create GridViewFragment to place inside MainActivity
    transition is not correct, will do dynamic mapping of transition name later

--- Gallery/app/src/main/java/com/rachelzhang/gallery/DetailViewActivity.java
+++ Gallery/app/src/main/java/com/rachelzhang/gallery/DetailViewActivity.java
@@ -2,12 +2,18 @@ package com.rachelzhang.gallery;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 
 public class DetailViewActivity extends FragmentActivity {
+  private ViewPager mDetailViewPager;
+  private DetailViewPagerAdapter mPagerAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.detail_view_activity);
+    mDetailViewPager = findViewById(R.id.detail_view_pager);
+    mPagerAdapter = new DetailViewPagerAdapter(getSupportFragmentManager());
+    mDetailViewPager.setAdapter(mPagerAdapter);
   }
 }
---
+++ Gallery/app/src/main/java/com/rachelzhang/gallery/GridViewFragment.java
@@ -0,0 +1,20 @@
+package com.rachelzhang.gallery;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GridViewFragment extends Fragment {
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.grid_view_fragment, container, true);
+
+  }
+}
